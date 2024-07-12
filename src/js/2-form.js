822e027d6cae64a34d4316bcbbb059921be090f0
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-07-12 21:23:32 +0300

    add space

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -34,7 +34,7 @@ function handleSubmit(event) {
     event.currentTarget.reset();
     localStorage.removeItem(FEEDBACK_KEY);
   } else {
-    return alert('All form fields must be filled in');
+    return alert('All form fields must be filled in !!!');
   }
 }
 
